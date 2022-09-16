7a7ed787e28a10c0683c9e64a9248ac43e4b4206
张无忌 <anthony.chin@foxmail.com>
2022-09-16 14:31:27 +0800

    add deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,23 +1,29 @@
 import { defineConfig } from 'vitepress'
 
-export default defineConfig({
+module.exports = {
   title: '张无忌的Blog',
   description: '张无忌的Blog',
   appearance: true,
+  // theme: 'reco',
   base: '/blog/',
   lastUpdated: true,
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
 
   themeConfig: {
     siteTitle: '张无忌的博客',
     logo: '/logo.svg',
      // 顶部菜单
     nav: [
-      { text: '首页', link: '/index' },
-      { text: '我的笔记', link: '/daily' },
+      { text: '首页', link: '/index.md' },
+      { text: '我的笔记', link: '/daily/index.md' },
       { text: '前端总结', link: '/javascript' },
-      { text: '生活感悟', link: '/life' },
+      { text: '生活感悟', link: '/life/index.md' },
       { text: '我的掘金', link: 'https://juejin.cn/user/1926000101300776' },
-      { text: '关于我', link: '/me' }
+      { text: '关于我', link: '/me/index.md' }
     ],
     // 社交链接
     socialLinks: [{ icon: 'github', link: 'https://github.com/AnthonyCXJ' }],
@@ -42,4 +48,4 @@ export default defineConfig({
       // open: true
     }
   }
-})
+}
